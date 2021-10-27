60a9c3e6589d54ab23d78f6eb129ce02f6638489
TomTomKinch <kinchthomas@gmail.com>
2021-10-26 20:57:52 -0700

    Added Some Styling to Images, began grid layout

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './HomePage.css';
+import './Grid.css';
 import Wow from './Images/Create-Virtual-Events--Walkthrough--Gabi-.png';
 import Sam from './Images/1634838255518_w1801_h1801.jpg'
 import Thomas from './Images/1634838272411_w1285_h1285.jpg';
@@ -44,55 +45,71 @@ function App() {
 
 
   return (
-    <div className="Home">
-      <div className="Top">
-        <p>Starts on NOV 03, 2021 AT: 10:00 AM PDT</p>
-        <p>Create standout virtual events, minus the cost and stress.</p>
-        <p>Learn how to create immersive visually stunning virtual experiences where your video content is the centerpiece.</p>
-        <p>Days</p>
-        <p>Hours</p>
-        <p>Minutes</p>
-        <p>Seconds</p>
-        <img src={Wow}/>
-      </div>
-      <div className="Speakers">
-        <p>Our Speakers</p>
-        <p>Meet Your Event Experts</p>
-        <p>Brandlive's line-up of creative leaders are here to help make you biggest event dreams become a reality.</p>
-          {expertsArray.map((person) => {
-            return(
-              <div>
-                <img src={person.image}/>
-                <p>{person.name}</p>
-                <p>{person.title}</p>
-                <p>{person.description}</p>
-              </div>
-            );
-          })}
-      </div>
-      <div className="Video_Section">
-        <p>Our Product</p>
-        <p>FPO Title</p>
-        <p>Good descriptive writing creates an impression in the reader's mind of an event, a place, a person, or a thing that will set a mood.</p>
-        <p> PLACE HOLDER FOR VIDEO </p>
-      </div>
-      <div className="Customers">
-        <p>Our Happy Customers</p>
-        <p>Words That Make Us Blush</p>
-        {customersArray.map((brand) => {
-            return(
-              <div>
-                <img src={brand.image}/>
-              </div>
-            );
-          })}
-      </div>
-      <div className="Footer">
-        <img src={BrandliveLogo}/>
-        <p>TERMS AND CONDITIONS</p>
-        <p>PRIVACY POLICY</p>
-        <img src={BrandliveStream}/>
-      </div>
+    <div className="grid-container">
+      {/* <div className="Home"> */}
+        <div className="Top">
+          <div className="grid-event-time">
+            <p>Starts on NOV 03, 2021 AT: 10:00 AM PDT</p>
+            <p>Create standout virtual events, minus the cost and stress.</p>
+            <p>Learn how to create immersive visually stunning virtual experiences where your video content is the centerpiece.</p>
+            <p>Days</p>
+            <p>Hours</p>
+            <p>Minutes</p>
+            <p>Seconds</p>
+          </div>
+          <div className="grid-wow">
+            <img src={Wow} className="Wow-Logo"/>
+          </div>
+        </div>
+        <div className="Speakers">
+          <div className="Speakers-Title">
+            <p>Our Speakers</p>
+            <p>Meet Your Event Experts</p>
+            <p>Brandlive's line-up of creative leaders are here to help make you biggest event dreams become a reality.</p>
+          </div>
+          <div className="Speaker-Profile">
+              {expertsArray.map((person) => {
+                return(
+                  <div>
+                    <img src={person.image} className="Speaker-Image"/>
+                    <br></br>
+                    <span>{person.name}</span>
+                    <br></br>
+                    <span>{person.title}</span>
+                    <br></br>
+                    <span>{person.description}</span>
+                    <br></br>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        <div className="Video-Section">
+          <p>Our Product</p>
+          <p>FPO Title</p>
+          <p>Good descriptive writing creates an impression in the reader's mind of an event, a place, a person, or a thing that will set a mood.</p>
+          <p> PLACE HOLDER FOR VIDEO </p>
+        </div>
+        <div className="Customers-Section">
+          <p>Our Happy Customers</p>
+          <p>Words That Make Us Blush</p>
+          <div className="Customers">
+            {customersArray.map((brand) => {
+                return(
+                  <div>
+                    <img src={brand.image} className="Customer-Logos"/>
+                  </div>
+                );
+              })}
+            </div>
+        </div>
+        <div className="Footer">
+          <img src={BrandliveLogo} className="Brandlive-Logo"/>
+          <p>TERMS AND CONDITIONS</p>
+          <p>PRIVACY POLICY</p>
+          <img src={BrandliveStream} className="BrandliveStream-Logo"/>
+        </div>
+      {/* </div> */}
     </div>
   );
 }
